b297f3526c38cb812074ba83482d2c46c242c933
Divyank Trivedi <divyanktrivedi0@gmail.com>
2025-03-19 06:24:37 +0530

    Initial Commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      <p className='font-light text-gray-500 mt-24'>Made with 🧡️ by Divyank Trivedi</p>
+    </div>
+  )
+}
+
+export default Footer
